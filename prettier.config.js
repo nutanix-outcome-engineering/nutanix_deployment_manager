5b4e6142f1b07a6d690255b256bc657de79a4c34
Will Whitt <william.whitt@nutanix.com>
2023-01-10 17:24:59 -0500

    Some of the initial project structure files

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+    trailingComma: "es6",
+    tabWidth: 2,
+    semi: false,
+    singleQuote: true,
+    tabs: false,
+    arrowParens: "avoid",
+    printWidth: 9999,
+    jsxBracketSameLine: false
+  }
+  

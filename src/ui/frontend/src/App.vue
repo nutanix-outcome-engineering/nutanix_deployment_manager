02c087aaddc1ea7b5dce49e45ba85437dfac63d7
Eric Bunton <eric.bunton@nutanix.com>
2023-01-20 21:04:37 +0000

    Add UI bits

---
+++ src/ui/frontend/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Application from './layouts/Application.vue';
+</script>
+
+<template>
+<component :is="Application">
+  <!-- <router-view /> -->
+</component>
+</template>
--- src/ui/ui.js
+++ src/ui/ui.js
@@ -6,10 +6,26 @@ const http = require('http')
 const config = require(`../lib/config`)
 const { registerAPIHandlers } = require('./api')
 
+const path = require('path')
 const app = express()
 
+/** Register API routes and eov middleware */
 registerAPIHandlers(app)
 
+/** Register UI Bundle Routes */
+app.use('/assets', express.static(path.resolve(__dirname,'frontend/dist/assets')))
+app.use('/', express.static(path.resolve(__dirname,'frontend/dist')))
+console.log(path.resolve(__dirname,'frontend/dist'))
+app.use('/*', express.static(path.resolve(__dirname, 'frontend/dist'), {
+  maxAge: '2h',
+  setHeaders: (res, path) => {
+      if (express.static.mime.lookup(path) === 'text/html') {
+          res.setHeader('Cache-Control', 'no-store')
+      }
+  }
+}))
+
+/** Register default error handler */
 app.use((err, req, res, next) => {
   res.status(err.status || 500).json({
     message: err.message
